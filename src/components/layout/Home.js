4c25835add08475d5b8bc364a89e1489cc4099a7
Forrest Filler <forrestfiller@users.noreply.github.com>
2017-04-08 09:53:17 -0400

    removed Account from home layout

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Tasks, Categories, Account } from '../containers'
+import { Tasks, Categories } from '../containers'
 
 class Home extends Component {
 	render(){
